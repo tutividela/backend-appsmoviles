29980f927838fa5788bf487e33b7062b343b4e97
agareval <angel.arevalo@connectis-ict.com.ar>
2023-08-18 19:31:55 -0300

    backend

---
+++ README.md
@@ -0,0 +1,14 @@
+# Backend MS Imágenes
+Back end del proyecto de aplicaciones móviles.
+Brinda soporte a la aplicación realizada por el grupo 2.
+
+Consideraciones y puntos fuertes del proyecto:
+- Las imagenes se guardan en el file system.
+- Se ofrece la funcionalidad de poder guardar varias fotos por categoría.
+- Login seguro con la cuenta de Google.
+- Vista amigable de las fotos.
+- Permite subida tanto de fotos con geolocalización como sin.
+- Filtros funcionales para filtrar familias entre Apellido, Barrio, Partido, Provincia.
+
+Presentación hecha el dia de la clase:
+https://docs.google.com/presentation/d/1BgvU1sRns1wf7qytpuRCP_Ij46etm5aD/edit?usp=sharing&ouid=113816484951693097048&rtpof=true&sd=true
---
+++ models/family.js
@@ -0,0 +1,79 @@
+  
+const mongoose = require('mongoose')
+
+const FamilySchema = new mongoose.Schema({
+  
+  familyId: String,
+
+  // Censo Construccion
+  PreCC_croquisE1: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PreCC_delanteCasa: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PreCC_bano: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PreCC_contratoAsignacion: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PreCC_fichaInspeccionPozos: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+
+  PostCC_modulo: [{
+    path: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PostCC_familiaDentro: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PostCC_higienizacion: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PostCC_cartaDonacion: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+  PostCC_cartaCesionImagen: [{
+    imageName: String,
+    latitude: Number,
+    longitude: Number,
+  }],
+
+})
+
+module.exports = mongoose.model('Family', FamilySchema)
+
+/*Fotos Pre CC:
+- Croquis encuesta 1 (vivienda, terreno y obstáculos constructivos).
+- Foto familia delante de la casa.
+- Baño actual. (obligatoria)
+·  Inodoro
+·  Pozo/balde
+- Contrato de asignación firmado.
+- Ficha inspección de pozos (incluye croquis con pozo y obstáculos
+constructivos)
+Post CC:
+- Módulo Sanitario por dentro
+- Familia dentro del MS terminado
+- Niños/as y adultos cepillándose los dientes/lavándose las manos dentro del MS
+- Foto carta de donación del MS
+- Foto carta cesión de imagen*/
